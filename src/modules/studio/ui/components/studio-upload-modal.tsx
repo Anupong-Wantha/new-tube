9fa678baf0afacb184427e1aa94b4f888f3a2754
Anupong-Wantha <jp0951269346@gmail.com>
2025-04-22 15:37:15 +0700

    Video-thumbnails

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -6,8 +6,11 @@ import { trpc } from "@/trpc/client";
 import { Loader2Icon, PlusIcon } from "lucide-react"
 import { toast } from "sonner";
 import { StudioUploader } from "./studio-uploader";
+import { useRouter } from "next/navigation";
+
 
 const StudioUploadModal = () => {
+  const router = useRouter();
   const utils = trpc.useUtils();
   const create = trpc.videos.create.useMutation({
     onSuccess:()=>{
@@ -18,6 +21,14 @@ const StudioUploadModal = () => {
       toast.error("Something went wrong");
     }
   });
+
+  const onSuccess = () =>{
+    if(!create.data?.video.id) return;
+
+    create.reset();
+    router.push(`/studio/videos/${create.data.video.id}`);
+
+  };
   
   return (
     <>
@@ -28,7 +39,7 @@ const StudioUploadModal = () => {
 
     >
       { create.data?.url 
-      ?<StudioUploader endpoint={create.data.url} onSuccess={()=>{}}/>
+      ?<StudioUploader endpoint={create.data.url} onSuccess={onSuccess}/>
 
       : <Loader2Icon/>
       }
---
+++ src/modules/studio/ui/views/video-view.tsx
@@ -0,0 +1,14 @@
+import { FromSection } from "../sections/from-section";
+
+interface PageProps {
+    videoId: string;
+
+};
+
+export const VideoViews =({videoId}:PageProps)=>{
+    return(
+        <div className="px-4 pt-2.5 pb-32 max-w-screen-lg  mr-auto ">
+            <FromSection videoId={videoId}/>
+        </div>
+    )
+}
