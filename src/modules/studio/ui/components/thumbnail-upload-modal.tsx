8dcfbe3011812f2d873c78317fe09a3403e7417a
Anupong-Wantha <jp0951269346@gmail.com>
2025-05-07 14:24:41 +0700

    fix

--- src/modules/studio/ui/components/thumbnail-upload-modal.tsx
+++ src/modules/studio/ui/components/thumbnail-upload-modal.tsx
@@ -5,49 +5,41 @@ import { UploadButton } from "@/lib/uploadthing";
 import { trpc } from "@/trpc/client";
 
 interface ThumbnailUploadModalProps {
-    videoId: string;
-    open: boolean;
-    onOpenChange:(open:boolean)=>void;
-};
+  videoId: string;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
 
 export const ThumbnailUploadModal = ({
-    videoId,
-    onOpenChange,
-    open,
-}:ThumbnailUploadModalProps) =>{
-    const utils = trpc.useUtils();
+  videoId,
+  onOpenChange,
+  open,
+}: ThumbnailUploadModalProps) => {
+  const utils = trpc.useUtils();
 
-    const onUploadComplete = () =>{
-        utils.studio.getMany.invalidate();
-        utils.studio.getOne.invalidate({id: videoId});
-        onOpenChange(false);
-    }
+  const onUploadComplete = () => {
+    utils.studio.getMany.invalidate();
+    utils.studio.getOne.invalidate({ id: videoId });
+    onOpenChange(false);
+  };
 
-    return(
-        <ResponsiveModal
-         title="Upload a thumbnail"
-         open={open}
-         onOpenChange={onOpenChange}
-        >
-
-            
-            
-            <UploadButton 
-                endpoint="tumbnailUploader"
-                appearance={{
-                    button: "flex items-center gap-2 bg-black  px-4 py-2 rounded  w-100 ",
-                    
-                  }}
-                input={{ videoId }}
-                onClientUploadComplete={onUploadComplete}
-                onUploadError={(error) => {
-                    console.error("Upload error:", error);
-                }}
-                
-            />
-            
-            
-
-        </ResponsiveModal>
-    );
+  return (
+    <ResponsiveModal
+      title="Upload a thumbnail"
+      open={open}
+      onOpenChange={onOpenChange}
+    >
+      <UploadButton
+        endpoint="tumbnailUploader"
+        appearance={{
+          button: "flex items-center gap-2 bg-black  px-4 py-2 rounded  w-100 ",
+        }}
+        input={{ videoId }}
+        onClientUploadComplete={onUploadComplete}
+        onUploadError={(error) => {
+          console.error("Upload error:", error);
+        }}
+      />
+    </ResponsiveModal>
+  );
 };
